5a8478595f7be09b46939e1072db172649657bfb
Kenny <kenny55660955@gmail.com>
2021-06-26 15:13:58 +0800

    Initial Commit

---
+++ PracticeInstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeInstagram
+//
+//  Created by Kenny on 2021/6/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
