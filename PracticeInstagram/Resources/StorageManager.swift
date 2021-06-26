c500ffd70f38d576c7676433f58c06db6255237a
Kenny <kenny55660955@gmail.com>
2021-06-26 16:23:06 +0800

    feat: Add Firebase pods

---
+++ PracticeInstagram/Resources/StorageManager.swift
@@ -0,0 +1,15 @@
+//
+//  StroageManager.swift
+//  PracticeInstagram
+//
+//  Created by Kenny on 2021/6/26.
+//
+
+import Foundation
+import FirebaseStorage
+
+class StorageManager {
+    
+    static let shard = StorageManager()
+    
+}
