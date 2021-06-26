42bd856c14bce66dba0acb8b908c7709171a9dc3
Kenny <kenny55660955@gmail.com>
2021-06-26 21:39:41 +0800

    feat: Add Register method

--- PracticeInstagram/Resources/AuthManager.swift
+++ PracticeInstagram/Resources/AuthManager.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import FirebaseDatabase
+import FirebaseAuth
 
 class AuthManager {
     // MARK: - Properties
@@ -14,11 +14,52 @@ class AuthManager {
     
     // MARK: - methods
     
-    func registerNewUser(userName: String, email: String, password: String) {
+    func registerNewUser(userName: String, email: String, password: String, completion: @escaping (Bool) -> Void) {
+        // Check if userName is Available
+        // Check if email is available
+        // Create account
+        // Insert account to database
+        DataBaseManager.shared.canCreateNewUser(with: email, userName: userName) { result in
+            if result {
+                Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                    // KennyToDo: - 這段教學有問題待修
+//                    guard error == nil, result != nil else {
+//                        completion(false)
+//                        return
+//                    }
+                    // Insert Data to database
+                    DataBaseManager.shared.insertNewUser(with: email, username: userName) { result in
+                        if result {
+                            completion(true)
+                            return
+                        } else {
+                            completion(false)
+                            return
+                        }
+                    }
+                }
+            } else {
+                completion(false)
+                return
+            }
+        }
         
     }
     
-    func loginUser(userName: String?, email: String?, password: String?) {
+    func loginUser(userName: String?, email: String?, password: String, completion: @escaping ((Bool) -> Void)) {
         
+        if let email = email {
+            // email Login
+            Auth.auth().signIn(withEmail: email, password: password) { result, error in
+                guard result != nil, error == nil else {
+                    completion(false)
+                    return
+                }
+                completion(true)
+            }
+             
+        } else if let userName = userName {
+            // username Login
+        }
     }
 }
--- PracticeInstagram/Resources/DataBaseManager.swift
+++ PracticeInstagram/Resources/DataBaseManager.swift
@@ -6,6 +6,31 @@
 //
 
 import Foundation
-import FirebaseAuth
+import FirebaseDatabase
+
+class DataBaseManager {
+    
+    static let shared = DataBaseManager()
+    
+    private let database = Database.database().reference()
+    // MARK: - Method
+    func canCreateNewUser(with email: String, userName: String, completion: @escaping (Bool) -> Void) {
+        completion(true)
+    }
+    
+    func insertNewUser(with email: String, username: String, completion: @escaping (Bool) -> Void) {
+        
+        database.child(email.safeDataBaseKey()).setValue(["username": username]) { error, _ in
+            if error == nil {
+                // successded
+                completion(true)
+            } else {
+                // failed
+                completion(false)
+                return
+            }
+        }
+    }
+}
 
 
