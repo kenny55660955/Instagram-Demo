03d50e0e1f78fbbc09f26f87e0bd3facf5d2d1ac
Kenny <kenny55660955@gmail.com>
2021-06-28 17:54:50 +0800

    feat: competion ProEditVC

---
+++ PracticeInstagram/VIews/CollectionView Headers/ProfileInfoHeaderCollectionReusableView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileInfoHeaderCollectionReusableView.swift
+//  PracticeInstagram
+//
+//  Created by Kenny on 2021/6/28.
+//
+
+import UIKit
+
+class ProfileInfoHeaderCollectionReusableView: UICollectionReusableView {
+    
+    static let identifier = "ProfileInfoHeaderCollectionReusableView"
+        
+}
---
+++ PracticeInstagram/VIews/CollectionView Headers/ProfileTabsCollectionReusableView.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileTabsCollectionReusableView.swift
+//  PracticeInstagram
+//
+//  Created by Kenny on 2021/6/28.
+//
+
+import UIKit
+
+class ProfileTabsCollectionReusableView: UICollectionReusableView {
+    
+    static let identifier = "ProfileTabsCollectionReusableView"
+      
+}
